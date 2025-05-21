bbc333e6064ad0fa56ba3310777b662981699cef
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-05-21 08:03:57 +0300

    implement getComputerChoice function to randomly select rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+function getComputerChoice() {
+  let value = Math.floor(Math.random() * 3) + 1;
+  if (value === 1) {
+    return 'rock';
+  } else if (value === 2) {
+    return 'paper';
+  } else if (value === 3) {
+    return 'scissors';
+  } else {
+    return 'Something went wrong';
+  }
+}
+
+console.log(getComputerChoice());
