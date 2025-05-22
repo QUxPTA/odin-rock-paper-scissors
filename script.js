f871a00ce06e99f248c485d4f2b7bd5e92ddf71c
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-05-22 22:41:16 +0300

    implement playRound function to determine the outcome of a rock-paper-scissors game

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ function getHumanChoice() {
 }
 
 console.log(getHumanChoice());
+
+function playRound(computerChoice, humanChoice) {
+  if (humanChoice === 'rock' && computerChoice === 'paper') {
+    return 'Computer chose Paper: You lose! Paper beats Rock';
+  } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    return 'Computer chose Scissors: You win! Rock beats Scissors';
+  } else if (humanChoice === 'rock' && computerChoice === 'rock') {
+    return "Computer chose Rock: It's a tie! Rock vs Rock";
+  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    return 'Computer chose Scissors: You lose! Scissors beats Paper';
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    return 'Computer chose Rock: You win! Paper beats Rock';
+  } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+    return "Computer chose Paper: It's a tie! Paper vs Paper";
+  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    return 'Computer chose Rock: You lose! Rock beats Scissors';
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    return 'Computer chose Paper: You win! Scissors beats Paper';
+  } else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
+    return "Computer chose Scissors: It's a tie! Scissors vs Scissors";
+  } else {
+    return 'Invalid choice. Please choose Rock, Paper, or Scissors.';
+  }
+}
